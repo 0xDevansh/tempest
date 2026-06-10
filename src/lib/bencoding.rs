7aacf648c31af63407ead3fbe4b57b8fe6d24ab3
Devansh <devanshkandpal54@gmail.com>
2026-06-10 10:32:28 +0530

    temp commit

--- src/lib/bencoding.rs
+++ src/lib/bencoding.rs
@@ -1,5 +1,7 @@
 use std::collections::BTreeMap;
 
+/// Representation of a value that can be bencoded
+#[derive(Debug)]
 pub enum Bencodable {
     Int (i32),
     String(String),
@@ -7,21 +9,42 @@ pub enum Bencodable {
     Dict(BTreeMap<String, Bencodable>)
 }
 
-pub fn encode(data: &Bencodable) -> String {
-    match data {
-        Bencodable::String(string) => {
-            format!("{}:{}", string.len(), string)
-        },
-        Bencodable::Int(num) => {
-            format!("i{}e", num)
-        },
-        Bencodable::List(list) => {
-            let elements = list.iter().map(|x| encode(x)).fold(String::new(), |a, b| a + &b);
-            format!("l{}e", elements)
-        },
-        Bencodable::Dict(dict) => {
-            let elements = dict.iter().map(|(k, v)| format!("{}{}", k, encode(v))).fold(String::new(), |a, b| a + &b);
-            format!("d{}e", elements)
+pub enum Token {
+    /// `i`
+    IntBegin,
+    /// `l`
+    ListBegin,
+    /// `d`
+    DictBegin,
+    /// `e`
+    End,
+    /// `:`
+    Colon,
+    /// any number
+    Number(i32),
+}
+
+fn make_token(chars: &Vec<char>, index: &mut usize) -> Token {
+    
+}
+
+impl Bencodable {
+    pub fn encode(&self) -> String {
+        match self {
+            Bencodable::String(string) => {
+                format!("{}:{}", string.len(), string)
+            },
+            Bencodable::Int(num) => {
+                format!("i{}e", num)
+            },
+            Bencodable::List(list) => {
+                let elements = list.iter().map(|x| x.encode()).fold(String::new(), |a, b| a + &b);
+                format!("l{}e", elements)
+            },
+            Bencodable::Dict(dict) => {
+                let elements = dict.iter().map(|(k, v)| format!("{}{}", k, v.encode())).fold(String::new(), |a, b| a + &b);
+                format!("d{}e", elements)
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ use std::collections::BTreeMap;
 
 use lib::bencoding::Bencodable;
 
-use crate::lib::bencoding::encode;
-
 fn main() {
     println!("Hello, world!");
     let data: Bencodable = Bencodable::Dict(BTreeMap::from([
         ("hello".to_owned(), Bencodable::String("world".to_owned())),
         ("foo".to_owned(), Bencodable::Int(42))
     ]));
-    let encoded = encode(&data);
+    let encoded = data.encode();
     println!("{}", encoded);
 }
